8b1d16dd9e2223d375db36171f0305ee86b17151
Hasib Rahman <86181823+hasibadeez@users.noreply.github.com>
2023-01-15 19:09:12 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -66,7 +66,7 @@ def stats(update, context):
 
 def start(update, context):
     if config_dict['DM_MODE']:
-        start_string = 'Welcome | Z BOT is ready for you.\n' \
+        start_string = 'Welcome | Aadeez BOT is ready for you.\n' \
                        'Thanks for starting me in DM.\n' \
                        'Now I can send all of your files and links here.\n'
     else:
@@ -74,7 +74,7 @@ def start(update, context):
                        'I can Mirror all your links To Google Drive! \n' \
                        'Unfortunately you are not authorized!\n' \
                        'Please deploy your own BOT!' \
-                       'Created With Love by @Z_Mirror . \n' \
+                       'Created With Love by @aadeezmirror_bot . \n' \
                        'Thank You!'
     sendMessage(start_string, context.bot, update.message)
 
